e29bbb495449b669f27b915665f87cb2e4aceee8
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-06-25 21:09:55 +0100

    Half made program, no need to finish.

--- Chapter 1 - Getting Started/Cal.cpp
+++ Chapter 1 - Getting Started/Cal.cpp
@@ -2,5 +2,8 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("");
+    printf("\nSun\tMon\tTues\tWed\tThurs\tFri\tSat\n");
+    printf("\t\t\t\t1\t2\t3\n");
+    printf("4\t5\t6\t7\t8\t9\t10\n");
+    getchar();
 };
