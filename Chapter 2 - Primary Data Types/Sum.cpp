4fee580d62d90ab1b6db5a552f4c3c6b17c4e860
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-07-02 14:39:25 +0100

    First challenge completed

---
+++ Chapter 2 - Primary Data Types/Sum.cpp
@@ -0,0 +1,15 @@
+/*
+* Author: Matthew Thompson
+* Desc: Challenge 1 in Chapter 2
+*/
+
+#include <stdio.h>
+
+int main(void) {
+  int a = 5;
+  int b = 1;
+  int x = 10;
+  int y = 5;
+
+  printf("The total is: %d", (a - b) + (x - y));
+}
