92d90aa10e64a019bd110b45695bffcd3d6e77c2
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-07-02 14:47:45 +0100

    Completed second challenge and renamed files

---
+++ Chapter 2 - Primary Data Types/InputCalc.cpp
@@ -0,0 +1,27 @@
+/*
+* Author: Matthew Thompson
+* Desc: Challenge 2 in Chapter 2
+*/
+
+#include <stdio.h>
+
+int main(void) {
+  int iUserA = 0;
+  int iUserB = 0;
+  int iUserX = 0;
+  int iUserY = 0;
+
+  printf("Give me a number for A:");
+  scanf("%d", &iUserA);
+
+  printf("Give me a number for B:");
+  scanf("%d", &iUserB);
+
+  printf("Give me a number for X:");
+  scanf("%d", &iUserX);
+
+  printf("Give me a number for Y:");
+  scanf("%d", &iUserY);
+
+  printf("The answer is: %d", (iUserA - iUserB) + (iUserX - iUserY));
+}
