d8ff96966e2385b5018156a8b9cf85b139c46d10
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-07-26 21:31:19 +0100

    Altered Function Prototypes Added code to main function Written Main Display function Written first switch statement in Questions function

--- Chapter 5 - Structured Programming/TriviaGame.cpp
+++ Chapter 5 - Structured Programming/TriviaGame.cpp
@@ -1,6 +1,6 @@
 /* 
 * App name: Trivia
-* Desc: Displays topics and asks a question based on selected toping
+* Desc: Displays topics and asks a question based on selected topic
 * Date Modified: 26/07/2018
 * Author: Matthew Thompson
 */
@@ -10,10 +10,60 @@
 #include <time.h>
 
 void MainDisplay(void);
-void Questions(void);
-int AnswerTracker(void);
+void Questions(int Category);
+int AnswerTracker(bool);
 void EndingDisplay(void);
 
 int main(void) {
+    system("clear");
+    MainDisplay();
     return 0;
+}
+
+void MainDisplay(void) {
+    int iSelection = 0;
+    printf("\n\tTRIVIA GAME\n");
+    printf("\n\tCategories:");
+    printf("\n\tAnimal Trivia: Enter \'1\'");
+    printf("\n\tVehicle Trivia: Enter \'2\'");
+    printf("\n\tHistory Trivia: Enter \'3\'");
+    printf("\n\tMovie Trivia: Enter \'4\'\n\n");
+    printf("Selection: ");
+    
+    scanf("%d", &iSelection);
+    system("clear");
+    Questions(iSelection);
+}
+
+void Questions(int Category) {
+    switch(Category) {
+        default:
+            printf("Error: Not a valid option");
+            MainDisplay(); 
+        break;
+        case 1:
+            int iSelection = 0;
+
+            do {
+                if(!(iSelection >= 1 && iSelection <= 4)) { 
+                    printf("\nYou need to select a valid option\n"); 
+                }
+                printf("\n\tAnimal Trivia Question.");
+                printf("\n\n\tWhat spiny venous fish, common in home aquariums, has become an invasive species in the Caribbean Sea and U.S. Atlantic coastal waters?\n");
+                printf("1. Lion Fish\n");
+                printf("2. Cat Fish\n");
+                printf("3. Gold Fish\n");
+                printf("4. Clown Fish\n");
+                scanf("%d", &iSelection);
+                system("clear");
+            } while(!(iSelection >= 1 && iSelection <= 4));
+
+            if(iSelection == 1) {
+                AnswerTracker(true);
+            }
+            else {
+                AnswerTracker(false);
+            }
+        break;
+    } 
 }
