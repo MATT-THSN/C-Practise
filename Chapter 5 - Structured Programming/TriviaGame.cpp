a35fec77cafe347126ded90d916798287e86ce1d
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-07-26 20:34:03 +0100

    Setup document, written function prototypes

---
+++ Chapter 5 - Structured Programming/TriviaGame.cpp
@@ -0,0 +1,19 @@
+/* 
+* App name: Trivia
+* Desc: Displays topics and asks a question based on selected toping
+* Date Modified: 26/07/2018
+* Author: Matthew Thompson
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void MainDisplay(void);
+void Questions(void);
+int AnswerTracker(void);
+void EndingDisplay(void);
+
+int main(void) {
+    return 0;
+}
