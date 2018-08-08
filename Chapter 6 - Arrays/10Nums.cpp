7c66a504db6b4d1e0ba338f0b86437deda52a11f
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-08-08 13:25:19 +0100

    Began program dev

---
+++ Chapter 6 - Arrays/10Nums.cpp
@@ -0,0 +1,19 @@
+/* 
+* Programming Practise File
+* Desc: Store 10 Numbers
+* Date Modified: 08/08/2018
+* Author: Matthew Thompson
+*/
+
+/*
+    - Build a program that uses a single-dimension array to store 10 numbers input by a user.
+    - After inputting the numbers, the user should see a menu with two options to sort and print the 10 numbers in ascending or descending order.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+
+}
