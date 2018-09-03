8a8b904eb3c9e2d5cac28add180e12bd4bb9b9b4
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-09-03 14:37:08 +0100

    Basic Program layout

---
+++ Chapter 6 - Arrays/GPAs.cpp
@@ -0,0 +1,25 @@
+/* 
+* Programming Practise File
+* Desc: GPA average calculator
+* Date Modified: 03/09/2018
+* Author: Matthew Thompson
+*/
+
+/*
+    - Build a program that uses a single-dimension array to store 30 GPA values, insertable by the user.
+    - Everytime they enter a GPA, give the option to calculate their GPA average or enter another GPA
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//VARIABLES
+float fValues[30] = {0};
+
+//FUNCTIONS
+void ValInsert();
+void AvgCalc();
+
+int main(void) {
+
+}
