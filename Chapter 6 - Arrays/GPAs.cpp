644029e807211b2d5e6056900ad92d63263c3a95
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-09-03 16:16:30 +0100

    Finished program

--- Chapter 6 - Arrays/GPAs.cpp
+++ Chapter 6 - Arrays/GPAs.cpp
@@ -14,7 +14,7 @@
 #include <stdlib.h>
 
 //VARIABLES
-float fValues[30] = {0};
+float fValues[30] = {0.0};
 bool keepRunning = true;
 
 //FUNCTIONS
@@ -40,45 +40,91 @@ int main(void) {
         switch(iMenuSelector) 
         {
             case 1:
+            system("clear");
             ViewGPA();
             break;
 
             case 2:
+            system("clear");
             ValInsert();
             break;
 
             case 3:
+            system("clear");
             keepRunning = !keepRunning;
             break;
 
             default:
+            system("clear");
             printf("\n\n\tNot a valid option\n");
             break;
         }
     }
 }
 
+/*
+*   View GPA FUNCTION
+*   Views stored GPA values
+*   Returns void
+*/
 void ViewGPA(){
+    system("clear");
+    printf("\n\n\tHere are your values: ");
     for(int i = 0; i < 30; i++) {
         if(fValues[i] != 0) {
             printf("%.1f ", fValues[i]);
         }
     }
+    printf("\n");
 }
 
+/*
+*   GPA Average Calculation FUNCTION
+*   Calculates the GPA Average
+*   Returns void
+*/
+void AvgCalc(){
+    int tmp = 0;
+    float fCalc = 0.0;
+    for(int i = 0; i < 30; i++) {
+        if(fValues[i] != 0.0) {
+            tmp++;
+        }
+    }
+
+    int x = 0;
+    for(x = 0; x < tmp; x++) {
+        fCalc = fCalc + fValues[x];
+    }
+    printf("\n\tYour average is: %f\n\n", (fCalc / x));
+}
+
+/*
+*   Value Insert FUNCTION
+*   Inserts GPA values
+*   Returns void
+*/
 void ValInsert(){
     int tmp = 0;
-    do {
-        for(int i = 0; i < 30; i++) {
-            if(fValues[i] == 0) {
-                scanf("%f", &fValues[i]);
-            }
+    system("clear");
+    printf("\n\tInsert a GPA Value: ");
+    
+    for(int i = 0; i < 30; i++) {
+        if(fValues[i] == 0.0) {
+            scanf(" %f", &fValues[i]);
+            break;
         }
-        printf("\n\n\tDo you want to add another GPA?\n\t1 for Yes\n\t2 for No\n\n");
-        scanf("%d", &tmp);
-    } while(tmp == 0);
-}
+    }
 
-void AvgCalc(){
+    system("clear");
+    printf("\n\n\tDo you want to calculate your GPA average?\n\n\t1 for Yes\n\t2 for No\n\n\t:");
+    scanf("%d", &tmp);
+    switch (tmp) {
+        case 1:
+        AvgCalc();
+        break;
 
+        case 2:
+        break;
+    }
 }
