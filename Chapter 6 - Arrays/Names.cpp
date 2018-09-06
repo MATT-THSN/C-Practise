28c10bc5a4b6505edcd0c95108ebcaa0a1f42ab4
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-09-06 16:19:34 +0100

    Debug

--- Chapter 6 - Arrays/Names.cpp
+++ Chapter 6 - Arrays/Names.cpp
@@ -14,42 +14,48 @@
 #include <stdlib.h>
 
 //VARIABLES
-char aNames[50][5];
+char aNames[5][50];
 
 //FUNCTION PROTOTYPES
 void stringRead(int val);
 
+void debug();
+
 int main(void) {
+    int index = 0;
     int tmp = 0;
     while(true) {
-        switch (tmp) {
+        switch (index) {
             case 0: 
-                printf("\n\tAdd a name1: ");
-                scanf(" %s", &aNames[tmp++][0]);
-                stringRead(0);
+                printf("\nIMA HERO\n");
+                printf("\n\tAdd a name1:");
+                scanf(" %s", &aNames[0][tmp]);
+                debug();
+                //stringRead(0);
+                printf("\nIMA FINA\n");
             break;
 
             case 1: 
                 printf("\n\tAdd a name2: ");
-                scanf(" %s", &aNames[tmp++][1]);
+                scanf(" %s", &aNames[tmp][1]);
                 stringRead(1);
             break;
 
             case 2:
                 printf("\n\tAdd a name3: ");
-                scanf(" %s", &aNames[tmp++][2]);
+                scanf(" %s", &aNames[tmp][2]);
                 stringRead(2);
             break;
 
             case 3:
                 printf("\n\tAdd a name4: ");
-                scanf(" %s", &aNames[tmp++][3]);
+                scanf(" %s", &aNames[tmp][3]);
                 stringRead(3);
             break;
 
             case 4:
                 printf("\n\tAdd a name5: ");
-                scanf(" %s", &aNames[tmp++][4]);
+                scanf(" %s", &aNames[tmp][4]);
                 stringRead(4);
             break;
 
@@ -67,11 +73,26 @@ int main(void) {
 */
 void stringRead(int val) {
     for(int x = 0; x < 50; x++) {
-        printf("Before");
+        printf("B-");
         printf("%c", aNames[x][val]);
-        printf("After");
+        printf("-A");
     }
 }
 
-
-
+void debug() {
+    for(int x = 0; x < 50; x++) {
+        printf("%c", aNames[0][x]);
+    }
+    for(int x = 0; x < 50; x++) {
+        printf("%c", aNames[x][1]);
+    }
+    for(int x = 0; x < 50; x++) {
+        printf("%c", aNames[x][2]);
+    }
+    for(int x = 0; x < 50; x++) {
+        printf("%c", aNames[x][3]);
+    }
+    for(int x = 0; x < 50; x++) {
+        printf("%c", aNames[x][4]);
+    }
+}
