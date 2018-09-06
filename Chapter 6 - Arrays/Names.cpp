7babe502e930b6f35ace22b912235048a8960ea5
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-09-06 13:46:33 +0100

    Initilised program

---
+++ Chapter 6 - Arrays/Names.cpp
@@ -0,0 +1,22 @@
+/* 
+* Programming Practise File
+* Desc: Store 10 Numbers
+* Date Modified: 06/09/2018
+* Author: Matthew Thompson
+*/
+
+/*
+    - 2 dimentional array, stores up to five names. 
+    - Asks for the user to input a new name, also asks to view added names
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//VARIABLES
+
+//FUNCTION PROTOTYPES
+
+int main(void) {
+
+}
