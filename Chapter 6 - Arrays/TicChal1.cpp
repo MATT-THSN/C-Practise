b3c0a9535ed0d1c92890ef7b51ddcd0c6f8a5541
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-09-07 19:21:51 +0100

    Rewritten to use 2d array

--- Chapter 6 - Arrays/TicChal1.cpp
+++ Chapter 6 - Arrays/TicChal1.cpp
@@ -16,35 +16,41 @@
 
 //FUNCTION PROTOTYPES
 void displayBoard();
-int verifySelection(int, int);
+int verifySelection(int, int, int);
 void checkForWin();
 
 //GLOBAL VARIABLES
-char board[9];
+char board[3][3];
 char cWhoWon = ' ';
 int iCurrentPlayer = 0;
 
 int main(void) {
-    int x;
-    int iSquareNum = 0;
+    int y = 0;
+    int x = 0;
+    int iSquareX = 0;
+    int iSquareY = 0;
 
-    for( x = 0; x < 9; x++ ) { board[x] = ' '; }
+    for( y = 0; y < 3; y++ ) {
+        for( x = 0; x < 3; x++ ) { 
+            board[x][y] = ' '; 
+        }
+    }
     displayBoard();
 
     while ( cWhoWon == ' ') {
         printf("\n%c\n", cWhoWon);
         if (iCurrentPlayer == 1 || iCurrentPlayer == 0) {
             printf("\nPLAYER X\n");
-            printf("Enter an available square number (1-9): ");
-            scanf("%d", &iSquareNum);
-            if ( verifySelection(iSquareNum, iCurrentPlayer) == 1 ) { iCurrentPlayer = 1; }
+            printf("Enter an available square number (1-3 & 1-3): ");
+            scanf("%d%d", &iSquareX, &iSquareY);
+            if ( verifySelection(iSquareX, iSquareY, iCurrentPlayer) == 1 ) { iCurrentPlayer = 1; }
             else { iCurrentPlayer = 2; }
         }
         else {
             printf("\nPLAYER 0\n");
-            printf("Enter an available square number (1-9): ");
-            scanf("%d", &iSquareNum); 
-            if ( verifySelection(iSquareNum, iCurrentPlayer) == 1) {iCurrentPlayer = 2;}
+            printf("Enter an available square number (1-3 & 1-3): ");
+            scanf("%d%d", &iSquareX, &iSquareY); 
+            if ( verifySelection(iSquareX, iSquareY, iCurrentPlayer) == 1) {iCurrentPlayer = 2;}
             else {iCurrentPlayer = 1;}
         } //END IF
         displayBoard();
@@ -58,24 +64,24 @@ void displayBoard() {
     system("clear");
     printf("\n\t|\t|\n");
     printf("\t|\t|\n");
-    printf("%c\t|%c\t|%c\n", board[0], board[1], board[2]);
+    printf("%c\t|%c\t|%c\n", board[0][0], board[0][1], board[0][2]);
     printf("--------|-------|------\n");
     printf("\t|\t|\n");
-    printf("%c\t|%c\t|%c\n", board[3], board[4], board[5]);
+    printf("%c\t|%c\t|%c\n", board[1][0], board[1][1], board[1][2]);
     printf("--------|-------|------\n");
     printf("\t|\t|\n");
-    printf("%c\t|%c\t|%c\n", board[6], board[7], board[8]);
+    printf("%c\t|%c\t|%c\n", board[2][0], board[2][1], board[2][2]);
     printf("\t|\t|\n");
 } //END FUNCTION DEFINITION
 
 //FUNCTION DEFINITION
-int verifySelection(int iSquare, int iPlayer) {
-    if (board[iSquare - 1] == ' ' && (iPlayer == 1 || iPlayer == 0)) {
-        board[iSquare - 1] = 'X';
+int verifySelection(int iSquareX, int iSquareY, int iPlayer) {
+    if (board[iSquareX - 1][iSquareY - 1] == ' ' && (iPlayer == 1 || iPlayer == 0)) {
+        board[iSquareX - 1][iSquareY - 1] = 'X';
         return 0;
     }
-    else if (board[iSquare - 1] == ' ' && iPlayer == 2) {
-        board[iSquare - 1] = '0';
+    else if (board[iSquareX - 1][iSquareY - 1] == ' ' && iPlayer == 2) {
+        board[iSquareX - 1][iSquareY - 1] = '0';
         return 0;
     }
     else {
@@ -86,24 +92,25 @@ int verifySelection(int iSquare, int iPlayer) {
 //FUNCTION DEFINITION
 void checkForWin() {
     int catTotal;
-    int x;
+    int x = 0;
+    int y = 0;
 
-    if(board[0] == 'X' && board[1] == 'X' && board[2] == 'X') { cWhoWon = 'X'; }
-    else if(board[3] == 'X' && board[4] == 'X' && board[5] == 'X') { cWhoWon = 'X'; }
-    else if(board[6] == 'X' && board[7] == 'X' && board[8] == 'X') { cWhoWon = 'X'; }
-    else if(board[0] == 'X' && board[3] == 'X' && board[6] == 'X') { cWhoWon = 'X'; }
-    else if(board[1] == 'X' && board[4] == 'X' && board[7] == 'X') { cWhoWon = 'X'; }
-    else if(board[2] == 'X' && board[5] == 'X' && board[8] == 'X') { cWhoWon = 'X'; }
-    else if(board[0] == 'X' && board[4] == 'X' && board[8] == 'X') { cWhoWon = 'X'; }
-    else if(board[2] == 'X' && board[4] == 'X' && board[6] == 'X') { cWhoWon = 'X'; }
-    else if(board[0] == '0' && board[1] == '0' && board[2] == '0') { cWhoWon = '0'; }
-    else if(board[3] == '0' && board[4] == '0' && board[5] == '0') { cWhoWon = '0'; }
-    else if(board[6] == '0' && board[7] == '0' && board[8] == '0') { cWhoWon = '0'; }
-    else if(board[0] == '0' && board[3] == '0' && board[6] == '0') { cWhoWon = '0'; }
-    else if(board[1] == '0' && board[4] == '0' && board[7] == '0') { cWhoWon = '0'; }
-    else if(board[2] == '0' && board[5] == '0' && board[8] == '0') { cWhoWon = '0'; }
-    else if(board[0] == '0' && board[4] == '0' && board[8] == '0') { cWhoWon = '0'; }
-    else if(board[2] == '0' && board[4] == '0' && board[6] == '0') { cWhoWon = '0'; }
+    if(board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') { cWhoWon = 'X'; }
+    else if(board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') { cWhoWon = 'X'; }
+    else if(board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X') { cWhoWon = 'X'; }
+    else if(board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') { cWhoWon = 'X'; }
+    else if(board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X') { cWhoWon = 'X'; }
+    else if(board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') { cWhoWon = 'X'; }
+    else if(board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') { cWhoWon = 'X'; }
+    else if(board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') { cWhoWon = 'X'; }
+    else if(board[0][0] == '0' && board[0][1] == '0' && board[0][2] == '0') { cWhoWon = '0'; }
+    else if(board[1][0] == '0' && board[1][1] == '0' && board[1][2] == '0') { cWhoWon = '0'; }
+    else if(board[2][0] == '0' && board[2][1] == '0' && board[2][2] == '0') { cWhoWon = '0'; }
+    else if(board[0][0] == '0' && board[1][0] == '0' && board[2][0] == '0') { cWhoWon = '0'; }
+    else if(board[0][1] == '0' && board[1][1] == '0' && board[2][1] == '0') { cWhoWon = '0'; }
+    else if(board[0][2] == '0' && board[1][2] == '0' && board[2][2] == '0') { cWhoWon = '0'; }
+    else if(board[0][0] == '0' && board[1][1] == '0' && board[2][2] == '0') { cWhoWon = '0'; }
+    else if(board[2][2] == '0' && board[1][1] == '0' && board[2][0] == '0') { cWhoWon = '0'; }
     if(cWhoWon == 'X') {
         printf("\nX Wins!\n");
         return;
@@ -113,10 +120,12 @@ void checkForWin() {
         return;
     }
     //Check for CAT / draw game
-    for (x = 0; x < 9; x++) {
-        if(board[x] != ' ') {
-            catTotal += 1;
-        }
+    for (y = 0; y < 3; y++) {
+        for (x = 0; x < 3; x++) {
+            if(board[x][y] != ' ') {
+                catTotal += 1;
+            }
+    }
     }//END For Loop
     if (catTotal == 9) {
         cWhoWon = 'C';
