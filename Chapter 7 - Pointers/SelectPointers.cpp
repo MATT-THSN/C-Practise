505ab44bf28e45270fa2900dda05bde2f1d3d48b
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-09-08 16:28:12 +0100

    Initilised program

---
+++ Chapter 7 - Pointers/SelectPointers.cpp
@@ -0,0 +1,23 @@
+/* 
+* Programming Practise File
+* Desc: Pointers Challenge 2
+* Date Modified: 08/09/2018
+* Author: Matthew Thompson
+*/
+
+/*
+    - Create a program that allows the user to select one of the following four menu options:
+        * Enter a new integer value
+        * Prints the value of each pointer variable
+        * Prints the address of each nonpointer variable
+        * Prints the address of each pointer variable
+    - For this program, you need to create two variables: one integer data type and one pointer. 
+    Using indirection, assign any new integer value that the user enters through an appropriate pointer.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    
+}
