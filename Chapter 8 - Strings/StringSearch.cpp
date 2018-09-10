a0b5d7d9d809116e4b55fc654480e685843b79fc
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-09-10 14:15:32 +0100

    Finished program, unable to test for now - FUNCTION ERROR

--- Chapter 8 - Strings/StringSearch.cpp
+++ Chapter 8 - Strings/StringSearch.cpp
@@ -13,6 +13,34 @@
         * "ay put!"
 */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
 int main(void) {
-    
+    char *sentence = "When the going gets tough, the tough stay put!";
+    char *comparision[3][10] = { "Going", "tou", "ay put!" };
+
+    if (strstr(comparision[0], sentence) != NULL) {
+        printf("\n\tFound! %s is in the sentence!", comparision[0]);
+    }
+    else {
+        printf("\n\tDid not find. %s is not in the sentence", comparision[0]);
+    }
+
+    if(strstr(comparision[1], sentence) != NULL) {
+        printf("\n\tFound! %s is in the sentence!", comparision[1]);
+    }
+    else {
+        printf("\n\tDid not find. %s is not in the sentence", comparision[1]);
+    }
+
+    if(strstr(comparision[2], sentence) != NULL) {
+        printf("\n\tFound! %s is in the sentence!", comparision[2]);
+    }
+    else {
+        printf("\n\tDid not find. %s is not in the sentence", comparision[2]);
+    }
+
 }
