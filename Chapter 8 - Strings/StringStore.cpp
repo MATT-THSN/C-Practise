76842a265da772b1ceea17305fbecef7bd423bd8
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-09-10 20:04:00 +0100

    Program written -ISSUE: Ordering decending order not ascending.

--- Chapter 8 - Strings/StringStore.cpp
+++ Chapter 8 - Strings/StringStore.cpp
@@ -19,14 +19,53 @@
 #include <stdlib.h>
 #include <string.h>
 
-void strSort(const char *);
+void strSort(char state[4][11]);
 
 int main(void) {
     char names[4][11] = {"Florida", "Oregon", "California", "Georgia"};
+    printf("\n\tHere are the states: %s, %s, %s and %s", names[0], names[1], names[2], names[3]);
+    strSort(names);
+    printf("\n\tHere are the states SORTED: %s, %s, %s and %s", names[0], names[1], names[2], names[3]);
 }
 
-void strSort(const char * state) {
-    if(strcmp(&state[0], &state[1])) {
+void strSort(char state[4][11]) {
+    int count = 0;
+    char tmpStore[2][11];
 
-    }
+    do {
+        count = 0;
+        //COMP 1 & 2
+        if((strcmp(&state[0][0], &state[1][0]) > 0)) {
+            count++;
+        }
+        else if(strcmp(&state[0][0], &state[1][0]) < 0) { 
+            strcpy(tmpStore[0], state[1]);
+            strcpy(tmpStore[1], state[0]);
+            strcpy(state[0], tmpStore[0]);
+            strcpy(state[1], tmpStore[1]);
+        }
+        else { count++; }
+        //COMP 2 & 3
+        if(strcmp(&state[1][0], &state[2][0]) > 0) {
+            count++;
+        }
+        else if(strcmp(&state[1][0], &state[2][0]) < 0) {
+            strcpy(tmpStore[0], state[2]);
+            strcpy(tmpStore[1], state[1]);
+            strcpy(state[1], tmpStore[0]);
+            strcpy(state[2], tmpStore[1]);
+        }
+        else { count++; }
+        //COMP 3 & 4
+        if(strcmp(&state[2][0], &state[3][0]) > 0) {
+            count++;
+        }
+        else if(strcmp(&state[2][0], &state[3][0]) < 0) {
+            strcpy(tmpStore[0], state[3]);
+            strcpy(tmpStore[1], state[2]);
+            strcpy(state[2], tmpStore[0]);
+            strcpy(state[3], tmpStore[1]);
+        }
+        else { count++; }
+    } while (count != 3);
 }
