596c7e8a0262139eecf359e07daaee09b2a56962
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-09-11 19:59:28 +0100

    Initialised and completed first challenge

---
+++ Chapter 9 - Introduction to Data Structures/car.cpp
@@ -0,0 +1,28 @@
+/* 
+* Programming Practise File
+* Desc: Data Structures challenges
+* Date Modified: 11/09/2018
+* Author: Matthew Thompson
+*/
+
+/*
+    - Create a structure called 'car' with the following members (attributes):
+        * make
+        * model
+        * year
+        * miles 
+*/
+
+typedef struct car {
+    char make[20];
+    char model[20];
+    int year;
+    int miles;
+} car;
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    
+}
