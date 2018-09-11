b163ec68f4d71dbf1d6c74862ca54688a4685831
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-09-11 12:01:21 +0100

    Written up word program

--- Chapter 8 - Strings/WordFind.cpp
+++ Chapter 8 - Strings/WordFind.cpp
@@ -14,6 +14,65 @@
         * Use the strlen() function to ensure the user's input string is the same length as the hidden word.
 */
 
-int main(void) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+#include <string.h>
 
-}
+//Function Prototypes
+void checkAnswer(char *, char []);
+
+int main(void) {
+    char *strGame[5] = {
+        "ADELANGUAGEFERVZ0PIBMOU",
+        "ZBPOINTERSKLML00PMNOCOT",
+        "PODSTRINGGDIWHIEEICERLS",
+        "YVCPROGRAMMERWQKNULTHMD",
+        "UKUNIXFIMWXIZEQZINPUTEX"
+    };
+
+    char answer[80] = {0};
+    int displayed = 0;
+    int x;
+    int startTime = 0;
+    system("clear");
+
+    printf("\n\n\tWord Find\n\n");
+    startTime = time(NULL);
+
+    for(x = 0; x < 5; x++) {
+        /* DISPLAY TEXT FOR FEW SECONDS */
+        while(startTime + 3 > time(NULL)) {
+            if (displayed == 0) {
+                printf("\nFind a word in: \n\n");
+                printf("%s\n\n", strGame[x]);
+                displayed = 1;
+            } //End If
+        } // End while loop
+    
+    system("clear");
+    printf("\nEnter word found: ");
+    scanf("%s", answer);
+    checkAnswer(strGame[x], answer);
+
+    displayed = 0;
+    startTime = time(NULL);
+    
+    }// End for loop
+    return 0;
+} //End main
+
+void checkAnswer(char *string1, char string2[]) {
+    int x;
+    /* Convert answer to UPPER CASE to perform a valid comparison */
+    for (x = 0; x <= strlen(string2); x++) {
+        string2[x] = toupper(string2[x]);
+    }
+    if (strstr(string1, string2) != 0 && string2[0] != 0) {
+        printf("\nGreat job!\n");
+    }
+    else {
+        printf("\nSorry, word not found!\n");
+    }
+} // End Check Answer
