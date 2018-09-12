8e75cb528307fe73320808d67e02ce2fcb3b3925
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-09-12 13:03:13 +0100

    Completed challenge

--- Chapter 10 - Dynamic Memory Allocation/name.cpp
+++ Chapter 10 - Dynamic Memory Allocation/name.cpp
@@ -13,3 +13,23 @@
         all memory allocated.) Print the number of characters in the user's name to standard output.
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+    int numC = 0;
+    char *name;
+    //Allocates memory to name
+    name = (char *) calloc(20, sizeof(char *));
+
+    printf("\n\tEnter your name: ");
+    printf("\n\tInput: "); scanf("%s", name);
+    printf("\tYour name is: ");
+    //Loops through name until it hits null pointer
+    while(name[numC] != '\0') {
+        printf("%c", name[numC]);
+        numC++;
+    }
+    printf("\n\tThere are %d characters in your name", numC);
+}
