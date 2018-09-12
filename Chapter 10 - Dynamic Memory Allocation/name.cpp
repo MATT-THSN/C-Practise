e3ac819c4a17426d63ae399cabf3b7fcfee6f539
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-09-12 12:46:13 +0100

    Written up challenge note

---
+++ Chapter 10 - Dynamic Memory Allocation/name.cpp
@@ -0,0 +1,15 @@
+/* 
+* Programming Practise File
+* Desc: Data Structures challenges
+* Date Modified: 12/09/2018
+* Author: Matthew Thompson
+*/
+
+/*
+    -   Using the calloc() function, write a program that reads a user's name from standard
+        input. Use a loop to iterate through the memory allocated, counting the number of
+        characters in the user's name. The loop should stop when a memory segment is reached
+        that was not used for reading and storing the user's name. (Remember, calloc() initializes
+        all memory allocated.) Print the number of characters in the user's name to standard output.
+*/
+
---
+++ Chapter 10 - Dynamic Memory Allocation/phonebook.cpp
@@ -0,0 +1,14 @@
+/* 
+* Programming Practise File
+* Desc: Data Structures challenges
+* Date Modified: 12/09/2018
+* Author: Matthew Thompson
+*/
+
+/*
+    -   Create a phonebook program that enables users to enter names and phone numbers of
+        friends and acquintances. Create a structure to hold contact information, and use
+        calloc() to reserve the first memory segment. The user should be able to add or modify
+        phone book entries through a menu. Use the realloc() function to add contiguous memory
+        segments to the original memory block when a user adds a new book entry.
+*/
