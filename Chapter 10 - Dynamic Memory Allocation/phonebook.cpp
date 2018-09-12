230128f1173af5a68c25e61c3aa70d1ffddce8ee
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-09-12 18:11:59 +0100

    Cut back on program func for now

--- Chapter 10 - Dynamic Memory Allocation/phonebook.cpp
+++ Chapter 10 - Dynamic Memory Allocation/phonebook.cpp
@@ -44,15 +44,16 @@ int main(void) {
         printf("\n\tCONTACTS APP\n\n");
         printf("\t#1 Add a contact");
         printf("\n\t#2 Modify a contact");
-        printf("\n\t#3 View contacts");
-        printf("\n\t#4 QUIT");
+        //printf("\n\t#3 View contacts");
+        printf("\n\t#3 QUIT");
         printf("\n\tInput: ");
         scanf("%d", &menuSelc);
         switch (menuSelc) {
             case 1:
+                if(contsAdded >= 1) {
+                    reAlloc(contactsInstance, &currentAlloc);
+                }
                 addContact(contactsInstance, &contsAdded);
-                printf("%s", contactsInstance[contsAdded].name);
-                printf("%s", contactsInstance[contsAdded].number);
             break;
 
             case 2:
@@ -60,11 +61,6 @@ int main(void) {
             break;
 
             case 3:
-            printf("H-");
-                viewContact(contactsInstance, &contsAdded);
-            break;
-
-            case 4:
                 run = !run;
             break;
         }
@@ -72,7 +68,6 @@ int main(void) {
 }
 
 void reAlloc(contacts *contactInstance, int * currentAlloc) {
-    *currentAlloc += 1;
     contactInstance = (struct contacts*) realloc(currentAlloc, sizeof(struct contacts));
     if(contactInstance == NULL) {
         printf("\n\tUnable to reallocate memory");
@@ -84,14 +79,7 @@ void addContact(contacts * contactsInstance, int * contsAdded) {
     contactsInstance[*contsAdded].name = (char *) calloc(20, sizeof(char *)); 
     printf("\n\tAdd a name: "); scanf("%s", contactsInstance[*contsAdded].name);
     printf("\tAdd a number: "); scanf("%s", contactsInstance[*contsAdded].number);
-    if(*contsAdded >= 1) {
-        reAlloc(*&contactsInstance, *&contsAdded);
-    }
-}
-
-void viewContact(contacts * contactsInstance, const int * contsAdded) {
-    printf("A-");
-    for(int x = 0; x <= *contsAdded; x++) {
-        printf("\n\t#%d Name: %s Phone: %s", *contsAdded, contactsInstance[x].name, contactsInstance[x].number);
-    }
+    printf("Before inc");
+    *contsAdded = *contsAdded + 1;
+    printf("After inc");
 }
